2cb7f3c5201c411c226804e486ab447df2e74d87
EvilPhilin <kurdjaev_sergey@rambler.ru>
2024-08-20 09:58:22 +0200

    Modified serializer in posts#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
 
-    render json: @posts
+    render json: @posts.to_json(only: %i[title text])
   end
 
   # GET /posts/1
