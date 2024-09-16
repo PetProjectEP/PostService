791404f67b95900f29fb64e0149c8d45673bd95f
EvilPhilin <kurdjaev_sergey@rambler.ru>
2024-09-16 14:03:47 +0200

    Pulled user_service url to config

--- app/controllers/concerns/user_service_reqs.rb
+++ app/controllers/concerns/user_service_reqs.rb
@@ -1,13 +1,15 @@
 module UserServiceReqs
   extend ActiveSupport::Concern
-  require "http" 
 
-  GET_USER_BY_SESSION_URL = "http://localhost:3000/sessions/"
+  require "http"
+  require "yaml"
 
+  SESSIONS_URL = YAML.load_file(Rails.root.join("config", "external_urls.yml"))["sessions_urls"]
+  
   public
   included do
     def get_user_by_session(token)
-      response = HTTP.get(GET_USER_BY_SESSION_URL + token)
+      response = HTTP.get(SESSIONS_URL + token)
 
       # 0-idx is here because of how HTTP-gem parser works
       return eval(response.body.to_a[0]) 
