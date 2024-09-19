0f80119a7e5619ea58b162624eba0b2b14e3b335
EvilPhilin <kurdjaev_sergey@rambler.ru>
2024-09-19 09:53:02 +0200

    Added length validations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates_length_of :title, in: 1..64, message: "The title must be between 1 and 64 characters long"
+  validates_length_of :text, within: 1..2048, message: "The post must be between 1 and 2048 characters long"
 end
