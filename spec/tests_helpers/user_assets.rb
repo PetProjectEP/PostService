11afb33cd3cd312303520c781332f0275ca08ba6
EvilPhilin <kurdjaev_sergey@rambler.ru>
2024-09-27 10:51:06 +0200

    A bit of refactor for UserAssets

--- spec/tests_helpers/user_assets.rb
+++ spec/tests_helpers/user_assets.rb
@@ -1,12 +1,11 @@
 module UserAssets
-  def self.ensure_valid_user(user_params: 
-  {
-    nickname: "Nickname",
+  def self.ensure_valid_user(
+    nickname: "Nickname",                                
     name: "John",
     surname: "Doe",
     password: "qwerty123",
     password_confirmation: "qwerty123"
-  })
+  )
     user = User.find_by(nickname: "Nickname")
     user ||= User.create(user_params)
     return user
